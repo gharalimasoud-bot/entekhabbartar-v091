379b8ec8fee60a4ab855be2f6ec2f12a807805b8
Masoud gharali <masoudgharali@Masouds-MacBook-Air.local>
2026-07-24 13:57:59 +0330

    توضیح مناسب

---
+++ js/auth.js
@@ -0,0 +1,45 @@
+console.log("AUTH FILE LOADED");
+async function login(){
+
+    const username =
+        document.getElementById("username").value.trim();
+
+    const password =
+        document.getElementById("password").value;
+
+    document.getElementById("loginError").textContent = "";
+
+    try{
+
+        const result = await api(
+            "login",
+            "POST",
+            {
+                username,
+                password
+            }
+        );
+
+        console.log(result);
+
+        if(result.ok){
+
+            alert("ورود موفق");
+
+        }else{
+
+            document.getElementById("loginError").textContent =
+                result.error || "خطا در ورود";
+
+        }
+
+    }catch(err){
+
+        console.error(err);
+
+        document.getElementById("loginError").textContent =
+            "ارتباط با سرور برقرار نشد.";
+
+    }
+
+}
